7bf55bb7ed1f3bd0f0e17556b93f13fd4ed21908
brandon15811 <brandon15811@6c064850-2d82-11df-afb1-bd2833558206>
2010-03-12 22:26:04 +0000

    Removed Extra File

--- upload/ndex.php
+++
@@ -1,9 +0,0 @@
-Search Songs:<br><br>
-<form name="input" action="search.php" method="get"> Search: <input type="text" name="name" />
-<input type="submit" value="Search" />
-</form>
-
-Playlist ID:<br><br>
-<form name="input" action="playlist.php" method="get"> Playlist ID: <input type="text" name="playlist" /> 
-<input type="submit" value="Go" /> 
-</form> 
