e6a08bb65af8f77f9db4130c9fb9d5d41d719e3d
brandon15811 <brandon15811@6c064850-2d82-11df-afb1-bd2833558206>
2010-03-12 22:22:29 +0000

    First Commit

---
+++ trunk/upload/index.php
@@ -0,0 +1,10 @@
+Search Songs:<br><br>
+<form name="input" action="search.php" method="get"> Search: <input type="text" name="name" />
+<input type="submit" value="Search" />
+</form>
+
+Playlist ID:<br><br>
+<form name="input" action="playlist.php" method="get"> Playlist ID: <input type="text" name="playlist" /> 
+<input type="submit" value="Go" /> 
+</form> 
+<a href="popular.php">Popular Songs</a>
---
+++ trunk/upload/popular.php
@@ -0,0 +1,20 @@
+<?php
+include 'exec.php';
+$populardata = popular();
+$songlist = json_decode($populardata, true);
+#echo $songlist[result][Songs][SongName][0];
+foreach($songlist[result][Songs] as $val) {
+	#$formmvalue = str_shuffle($val[Name]);
+	#$formvalue = str_replace(" ", "", "$formmvalue");
+    echo "Song:".$val[Name]."<br><br>".
+    "Artist:".$val[ArtistName]."<br><br>";
+	#$streamjson = streamKey($val[SongID]);
+	#$streamdata = json_decode($streamjson, true);
+	#$buttonurl = $streamdata[result][result][streamServer];
+	#$buttonkey = $streamdata[result][result][streamKey];
+	echo "<a href=stream.php?songid=".$val[SongID].">Download</a><br><br><br> ";
+}
+
+
+
+?>
