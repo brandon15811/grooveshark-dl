a8bba480fb8754ec5fa1d5ff8121b987281003d8
brandon15811 <brandon15811@6c064850-2d82-11df-afb1-bd2833558206>
2010-06-01 19:57:53 +0000

    Added initial nightly files

---
+++ branches/popular.php
@@ -0,0 +1,78 @@
+<?php
+#ssession_start();
+include 'newexec.php';
+#include "notice.html";
+if (!isset($_GET["type"])) {
+	$gettype = "songs";
+} else {
+	$gettype = $_GET['type'];
+}
+#$session = $_SESSION['sessionid'];
+switch($gettype) {
+	case "artists":
+		$popularartistdata = popularGetArtists();
+		$artistlist = json_decode($popularartistdata, true);
+		#echo $songlist[result][Songs][SongName][0];
+		echo "<br>";
+		foreach($artistlist["result"]["artists"] as $val) {
+			#$formmvalue = str_shuffle($val[Name]);
+			#$formvalue = str_replace(" ", "", "$formmvalue");
+			#echo "Song:".$val["songName"]."<br><br>".
+			echo "Artist:".$val["artistName"]."<br><br><br>";
+			#$streamjson = streamKey($val[SongID]);
+			#$streamdata = json_decode($streamjson, true);
+			#$buttonurl = $streamdata[result][result][streamServer];
+			#$buttonkey = $streamdata[result][result][streamKey];
+			#echo "<a href=stream.php?songid=".$val["songID"].">Play</a><br><br><br>";
+		}
+	case "albums":
+		$popularalbumdata = popularGetAlbums();
+		$albumlist = json_decode($popularalbumdata, true);
+		#echo $songlist[result][Songs][SongName][0];
+		echo "<br>";
+		foreach($albumlist["result"]["albums"] as $val) {
+			#$formmvalue = str_shuffle($val[Name]);
+			#$formvalue = str_replace(" ", "", "$formmvalue");
+			echo "Album:".$val["albumName"]."<br><br>".
+			"Artist:".$val["artistName"]."<br><br><br>";
+			#$streamjson = streamKey($val[SongID]);
+			#$streamdata = json_decode($streamjson, true);
+			#$buttonurl = $streamdata[result][result][streamServer];
+			#$buttonkey = $streamdata[result][result][streamKey];
+			#echo "<a href=stream.php?songid=".$val["songID"].">Play</a><br><br><br>";
+		}
+	default:
+		$popularsongdata = popularGetSongs();
+		$songlist = json_decode($popularsongdata, true);
+		#echo $songlist[result][Songs][SongName][0];
+		echo "<br>";
+		foreach($songlist["result"]["songs"] as $val) {
+			#$formmvalue = str_shuffle($val[Name]);
+			#$formvalue = str_replace(" ", "", "$formmvalue");
+			echo "Song:".$val["songName"]."<br><br>".
+			"Artist:".$val["artistName"]."<br><br>";
+			#$streamjson = streamKey($val[SongID]);
+			#$streamdata = json_decode($streamjson, true);
+			#$buttonurl = $streamdata[result][result][streamServer];
+			#$buttonkey = $streamdata[result][result][streamKey];
+			echo "<a href=stream.php?songid=".$val["songID"].">Play</a><br><br><br>";
+		}
+	}
+
+?>
+<!-- Start 1FreeCounter.com code -->
+  
+  <script language="JavaScript">
+  var data = '&r=' + escape(document.referrer)
+	+ '&n=' + escape(navigator.userAgent)
+	+ '&p=' + escape(navigator.userAgent)
+	+ '&g=' + escape(document.location.href);
+
+  if (navigator.userAgent.substring(0,1)>'3')
+    data = data + '&sd=' + screen.colorDepth 
+	+ '&sw=' + escape(screen.width+'x'+screen.height);
+
+  document.write('<img alt="" border=0 hspace=0 '+'vspace=0 src="http://www.1freecounter.com/counter.php?i=53197' + data + '">');
+  </script>
+
+<!-- End 1FreeCounter.com code -->
---
+++ branches/search.php
@@ -0,0 +1,41 @@
+<?php
+#session_start();
+include 'newexec.php';
+#include "notice.html";
+#$session = $_SESSION['sessionid'];
+echo '<form name="input" action="search.php" method="get"> Search: <input type="text" name="name" /> <input type="submit" value="Search" /> </form> ';
+echo "<br><br>";
+if (isset($_GET['name'])) {
+$searchdata = searchSongs($_GET['name']);
+$songlist = json_decode($searchdata, true);
+#echo $songlist[result][Songs][SongName][0];
+foreach($songlist["result"]["songs"] as $val) {
+	#$formmvalue = str_shuffle($val[Name]);
+	#$formvalue = str_replace(" ", "", "$formmvalue");
+    echo "Song:".$val["songName"]."<br><br>".
+    "Artist:".$val["artistName"]."<br><br>";
+	#$streamjson = streamKey($val[SongID]);
+	#$streamdata = json_decode($streamjson, true);
+	#$buttonurl = $streamdata[result][result][streamServer];
+	#$buttonkey = $streamdata[result][result][streamKey];
+	echo "<a href=stream.php?songid=".$val["songID"].">Download</a><br><br><br> ";
+	#echo "<a href=".$val[SongID].".mp3>Download</a><br><br><br> ";
+}
+                          }
+?>
+<!-- Start 1FreeCounter.com code -->
+  
+  <script language="JavaScript">
+  var data = '&r=' + escape(document.referrer)
+	+ '&n=' + escape(navigator.userAgent)
+	+ '&p=' + escape(navigator.userAgent)
+	+ '&g=' + escape(document.location.href);
+
+  if (navigator.userAgent.substring(0,1)>'3')
+    data = data + '&sd=' + screen.colorDepth 
+	+ '&sw=' + escape(screen.width+'x'+screen.height);
+
+  document.write('<img alt="" border=0 hspace=0 '+'vspace=0 src="http://www.1freecounter.com/counter.php?i=53197' + data + '">');
+  </script>
+
+<!-- End 1FreeCounter.com code -->
