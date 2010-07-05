1ede520cdcc69b74479a6911fd39f5d13ee9e447
brandon15811 <brandon15811@6c064850-2d82-11df-afb1-bd2833558206>
2010-07-05 01:45:02 +0000

    Fixed code comments, and fixed parsing in playlist.php

--- playlist.php
+++ playlist.php
@@ -22,19 +22,19 @@ include 'exec.php';
 #include "notice.html";
 #$session = $_SESSION['sessionid'];
 echo "<br><br>";
-$listdata = playlist($_GET['playlist']);
+$listdata = playlistGetSongs($_GET['playlist']);
 $songlist = json_decode($listdata, true);
 #echo $songlist[result][Songs][SongName][0];
-foreach($songlist[result][Songs] as $val) {
-	$formmvalue = str_shuffle($val[Name]);
-	$formvalue = str_replace(" ", "", "$formmvalue");
-    echo "Song:".$val[Name]."<br><br>".
-    "Artist:".$val[ArtistName]."<br><br>";
+foreach($songlist['result']['songs'] as $val) {
+	/*$formmvalue = str_shuffle($val['songName']);
+	$formvalue = str_replace(" ", "", "$formmvalue");*/
+    echo "Song:".$val['songName']."<br><br>".
+    "Artist:".$val['artistName']."<br><br>";
 	#$streamjson = streamKey($val[SongID]);
 	#$streamdata = json_decode($streamjson, true);
 	#$buttonurl = $streamdata[result][result][streamServer];
 	#$buttonkey = $streamdata[result][result][streamKey];
-	echo "<a href=stream.php?songid=".$val[SongID].">Download</a><br><br><br> ";
+	echo "<a href=stream.php?songid=".$val['songID'].">Download</a><br><br><br> ";
 	
 };
 ?>
